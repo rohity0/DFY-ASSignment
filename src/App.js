4103099714584e945ec2d4f03380c8705bab4dc0
Rohit Yadav <101393562+rohity0@users.noreply.github.com>
2023-01-17 17:23:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,19 +9,6 @@ function App() {
     <div>
       <Map/>
     </div>
-    // <MapContainer  style={{width:"99vw" ,height: "99vh"}} center={[20.5937,78.9629]} zoom={6} scrollWheelZoom={true}>
-    // <TileLayer
-    //   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    //   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    // />
-    // {/* <Marker position={[51.505, -0.09]}>
-    //   <Popup>
-    //     A pretty CSS3 popup. <br /> Easily customizable.
-    //   </Popup>
-    // </Marker> */}
-  //   <input style={{zIndex: 1000}} type={"text"} placeholder="Hello" />
-  //   <Polygon pathOptions={purpleOptions} positions={multiPolygon} />
-  // </MapContainer>
   );
 }
 
